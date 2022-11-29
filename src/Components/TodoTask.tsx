3f19b4cf942243fd6c34d103a2f9b0a27eb93105
Aekkasitrun <aekkasitrun@gmail.com>
2022-11-29 11:09:57 +0700

    edit TodoTask

--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 const TodoTask = ({ task, completeTask }: Props) => {
   return (
-    <div className="flex justify-end">
-      <div className="flex items-stretch ">
-        <span className="p-3 border-solid border-2 border-gray-600 rounded-md">{task.taskName}</span>
-        <span className="p-3 border-solid border-2 border-gray-600 rounded-md">{task.deadline}</span>
+    <div >
+      <div className="flex">
+        <span className="flex-1 border-solid border-2 border-gray-600 rounded-md">{task.taskName}</span>
+        <span className="flex-1 border-solid border-2 border-gray-600 rounded-md">{task.deadline}</span>
         <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500"
         onClick={() => {
           completeTask(task.taskName);
